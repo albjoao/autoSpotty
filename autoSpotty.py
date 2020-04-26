1f0a3946fbb673dde0ab5c7a2b502a9bd7ccdb03
Joao Alberto de Faria <fariajoao92@gmail.com>
2020-04-26 10:25:07 -0400

    Create class to encapsulate playlist functionality

--- autoSpotty.py
+++ autoSpotty.py
@@ -4,21 +4,7 @@ import sys
 import spotipy
 import spotipy.util as util
 import json
-
-def track_id_helper(tracks, uri):
-    for track in tracks['items']:
-        uri.append(track['track']['uri'])
-
-def getTracks(playlistID):
-    '''Gets track uris from playlist ID'''
-    trackURIS = []
-    playlist = sp.playlist(playlistID)
-    tracks = playlist['tracks']
-    track_id_helper(tracks, trackURIS)
-    while tracks['next']:
-        tracks = sp.next(tracks)
-        track_id_helper(tracks, trackURIS)
-    return trackURIS
+from tracks import Playlist, UserPlaylist
 
 def getUserPlaylistID(username,name):
     '''Converts human readable name to spotify id'''
@@ -27,23 +13,6 @@ def getUserPlaylistID(username,name):
         if playlist['owner']['id'] == username and playlist['name'] == name:
             return playlist['id']
 
-def showTracks(trackUris):
-    '''Prints all tracks added'''
-    tracks = sp.tracks(trackUris)
-    for track in tracks['tracks']:
-        print(track['name'])
-    print()
-
-def addNewSongs(username, playlistID, newSongs):
-    playListURIS = getTracks(playlistID)
-    actuallyNew = [uri for uri in newSongs if not uri in playListURIS]
-    if actuallyNew:
-        print("Adding: ")
-        showTracks(actuallyNew)
-        sp.user_playlist_add_tracks(username, playlistID, actuallyNew)
-    else:
-        print('No New songs to Add')
-
 
 if __name__ == '__main__':
     username='thorriors'
@@ -51,9 +20,8 @@ if __name__ == '__main__':
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        discoverWeeklyUris = getTracks('37i9dQZEVXcXAN5Q9WscX1') 
-        bestWeekly = getUserPlaylistID(username, 'Best Weekly')
-        addNewSongs(username,bestWeekly,discoverWeeklyUris)
-
+        discoverWeekly = Playlist(sp, '37i9dQZEVXcXAN5Q9WscX1') 
+        bestWeekly = UserPlaylist(sp, username, 'Best Weekly')
+        bestWeekly.mergePlaylist(discoverWeekly)
     else:
         print("Can't get token for", username)
